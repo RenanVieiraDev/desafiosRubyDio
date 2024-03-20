73915936b1ec6c90afd84dca15c93175cc981317
Renan Vieira <renanvieira.andrade2@gmail.com>
2024-03-20 17:07:32 -0300

    desafio01

---
+++ Desafios/desafio01.rb
@@ -0,0 +1,10 @@
+print "digite seu nome:"
+nome = gets.chomp
+
+print "Digite seu sobrenome:"
+sobrenome = gets.chomp
+
+print "Digite sua idade:"
+idade = gets.chomp
+
+puts "Dados para cadastro ( nome: #{nome} #{sobrenome} , idade: #{idade} )"
